56aed118274816b23a670e3a073100091906e0c3
lakmali-vishaka <119029100+lakmali-vishaka@users.noreply.github.com>
2025-06-15 11:00:41 +0530

    adding community

--- Models/User.js
+++ Models/User.js
@@ -64,4 +64,5 @@ const userSchema = new mongoose.Schema(
 );
 
 
-module.exports = mongoose.model("User", userSchema);
+//module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.models.User || mongoose.model('User', userSchema);
